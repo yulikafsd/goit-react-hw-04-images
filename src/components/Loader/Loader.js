afd3efcc692887360b9536cafdaab25d96da5522
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-12-23 13:08:09 +0100

    bugfix style loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,13 +1,21 @@
+import css from './Loader.module.css';
 import { ThreeDots } from 'react-loader-spinner';
 
 export const Loader = () => (
-  <ThreeDots
-    height="80"
-    width="80"
-    radius="9"
-    color="#3F51B5"
-    ariaLabel="three-dots-loading"
-    wrapperStyle={{ margin: 'auto' }}
-    visible={true}
-  />
+  <div className={css.Loader}>
+    <ThreeDots
+      height="80"
+      width="80"
+      radius="9"
+      color="#3F51B5"
+      ariaLabel="three-dots-loading"
+      wrapperStyle={{
+        display: 'flex',
+        alignItems: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      }}
+      visible={true}
+    />
+  </div>
 );
