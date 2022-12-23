1a5bcba95ca24a0d8ca6937089a3e3dae7cd914a
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-12-23 01:19:32 +0100

    finishes app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,78 +1,92 @@
 import css from './App.module.css';
 import { Component } from 'react';
-import { ApiService } from 'services/ApiService';
-import { Searchbar } from 'components/Searchbar';
-import { ImageGallery } from 'components/ImageGallery';
-import { Button } from 'components/Button';
-import { Modal } from 'components/Modal';
-
-const apiService = new ApiService();
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchPictures } from 'services/ApiService';
+import { Searchbar, ImageGallery, Button } from 'components';
 
 export class App extends Component {
 
   state = {
-    loading: false,
-    largeImgSrc: '',
-    tags: '',
-    images: []
+    images: [],
+    query: '',
+    page: 0,
+    lastPage: 0,
+    status: 'idle',
   }
 
-  handleSubmit = async ({ newQuery }) => {
-    apiService.query = newQuery;
-    apiService.resetPage();
+  async componentDidUpdate(_, prevState) {
 
-    try {
-      this.setState({ loading: true });
-      const promise = await apiService.fetchPictures();
-      const { totalHits } = promise;
-      // const { hits } = promise;
+    const { query, page } = this.state;
 
-      if (totalHits < 1) {
-      // notifyFailure();
-      // clearGallery();
-        console.log(`nothing found`);
-      } else {
-        apiService.totalHits = totalHits;  
-        this.setState({ images: [...apiService.images], totalHits });
-        // clearGallery();
-        // notifySuccess(totalHits);
-        // checkLastPage();
-        console.log(`found ${totalHits} results => render`)
-      }
-      this.setState({ loading: false });
-    } catch (error) {
-      console.log(error);
+    if (prevState.page !== page && page !== 1) {
+
+      try {
+        const response = await fetchPictures(query, page);
+        this.setState(({images}) => ({ images: [...images, ...response.hits], status: 'resolved' }));
+        setTimeout(() => this.scroll(), 100);
+
+        } catch (error) {
+        this.setState({ status: 'rejected' });
+        console.log(error.message);
+      }      
+      
     }
-
   }
 
-  handleImageClick = ({ largeImageURL, alt }) => {
-    console.log(largeImageURL);
-    console.log(alt);
-    this.setState({ largeImgSrc: largeImageURL, tags: alt });
+  handleSubmit = async (event) => {
+    event.preventDefault();
 
+    const newQuery = event.target.elements.input.value.trim();
+    const page = 1;
+
+    if (newQuery === "") {
+      return Notify.warning('Search field is empty. Please, enter your request');
+    }
+    
+    this.setState({ query: newQuery, page: 1, images: [], status: 'pending' });
+    
+    try {
+      const response = await fetchPictures(newQuery, page);
+      const { totalHits, hits } = response;
+
+        if (totalHits === 0) {
+          this.setState({ lastPage: 1, status: 'rejected' });
+          Notify.failure('Sorry, there are no images matching your search request. Please try another request.');
+          return;
+      }
+      
+      const lastPage = Math.ceil(totalHits / 12);
+      this.setState({ lastPage, images: hits, status: 'resolved' });
+      Notify.success(`Hurray! ${totalHits} images found`);
+
+        } catch (error) {
+        this.setState({ status: 'rejected' });
+        console.log(error.message);
+    }     
   }
-
+  
   handleBtnClick = () => {
-  //    this.fetchImages(query);
+    this.setState(prevState => ({ page: prevState.page + 1, }));
   }
 
-  handleModalClose = () => {
-    this.setState({ largeImgSrc: '', tags: ''});
-  }
+  scroll = () => {
+    const { clientHeight } = document.documentElement;
+    window.scrollBy({
+      top: clientHeight - 180,
+      behavior: 'smooth',
+    });
+  };
 
   render() {
 
-    const { images, largeImgSrc, tags } = this.state;
-
+    const { images, page, lastPage, status } = this.state;
     return (
       <div className={css.App}>
-        <Searchbar onSubmit={this.handleSubmit} />
+        <Searchbar onSubmit={this.handleSubmit} isSubmitting={status === 'pending'}/>
         <main>
-          {this.state.loading && <p>LOADING.....</p>}
-          <ImageGallery images={images} onClick={this.handleImageClick} />
-          {apiService.totalHits > 12 && <Button onClick={this.handleBtnClick} />}
-          {largeImgSrc && <Modal onClose={this.handleModalClose} src={largeImgSrc} alt={tags} />}
+          {status === 'pending' && <p>LOADING.....</p>}
+          {status === 'resolved' && <ImageGallery images={images} />}
+          {page !== lastPage && <Button onClick={this.handleBtnClick} />}
         </main>
       </div>
   )}
