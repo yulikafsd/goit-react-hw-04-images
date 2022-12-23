3ecc15d2d388a46d889b706c7e4f287e161b9cf0
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-12-23 01:18:36 +0100

    move submit to app

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,31 +1,26 @@
 import css from './Searchbar.module.css';
-import { Component } from 'react';
 
-export class Searchbar extends Component {
-  handleFormSubmit = event => {
-    event.preventDefault();
-    const newQuery = event.target.elements.input.value.trim();
-    this.props.onSubmit({ newQuery });
-  };
+export const Searchbar = ({ onSubmit, isSubmitting }) => {
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={onSubmit}>
+        <button
+          type="submit"
+          className={css.SearchFormButton}
+          disabled={isSubmitting}
+        >
+          <span className={css.SearchFormButtonLabel}>Search</span>
+        </button>
 
-  render() {
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.handleFormSubmit}>
-          <button type="submit" className={css.SearchFormButton}>
-            <span className={css.SearchFormButtonLabel}>Search</span>
-          </button>
-
-          <input
-            className={css.SearchFormInput}
-            type="text"
-            name="input"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={css.SearchFormInput}
+          type="text"
+          name="input"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
